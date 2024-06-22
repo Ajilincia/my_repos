9e96a9e2a27528878b4f2786d843534e165a077c
Ajilincia <163734159+Ajilincia@users.noreply.github.com>
2024-06-22 19:19:18 +0530

    Update AppTest.java

--- maven_first_example/my-app/src/test/java/com/mycompany/app/AppTest.java
+++ maven_first_example/my-app/src/test/java/com/mycompany/app/AppTest.java
@@ -33,8 +33,8 @@ public class AppTest
      ****/
     public void testApp()
     {
-        assertTrue( false );
-    }
+        assertTrue( true );
+    
     public void testApp2()
     {
         assertTrue( true );
